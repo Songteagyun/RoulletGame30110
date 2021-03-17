7b257a3a9cfba414e6d063a7a8efc71f24abea97
Songteagyun <80800089+Songteagyun@users.noreply.github.com>
2021-03-17 11:03:19 +0900

    .

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotspeed = 0.0f;
+
+    void Update()
+
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotspeed = 0.0f;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            this.rotspeed = 20.0f;
+        }
+        transform.Rotate(0, 0, this.rotspeed);
+        this.rotspeed = this.rotspeed * 0.987f;
+    }
+   
+}
